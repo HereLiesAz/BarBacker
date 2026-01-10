2ac67ea3de09a729e468fee7d200109c36b374fc
Az <103241502+HereLiesAz@users.noreply.github.com>
2026-01-10 06:21:44 -0600

    Update vite-env.d.ts
    Signed-off-by: Az <103241502+HereLiesAz@users.noreply.github.com>

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,22 @@
 /// <reference types="vite/client" />
+
+declare module 'react' {
+  namespace JSX {
+    interface IntrinsicElements {
+      'md-filled-button': any;
+      'md-outlined-button': any;
+      'md-elevated-button': any;
+      'md-text-button': any;
+      'md-filled-text-field': any;
+      'md-icon': any;
+      'md-list': any;
+      'md-list-item': any;
+      'md-divider': any;
+      'md-dialog': any;
+      'md-icon-button': any;
+      'md-circular-progress': any;
+      'md-chip-set': any;
+      'md-filter-chip': any;
+    }
+  }
+}
